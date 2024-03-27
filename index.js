576ab71eedf7cc282de83b3f5230ef69f0a36435
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-03-27 03:06:45 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const multiply = (num1, num2) => num1 * num2;
+const num1 = 5;
+const num2 = 12;
+console.log(`The product of ${num1} and ${num2} is ${multiply(num1, num2)}.`);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(`The random integer is ${random}.`);
+
+const mod = num3 % num4;
+const num3 = 20;
+const num4 = 4;
+console.log(`The remainder of ${num3} divided by ${num4} is ${mod}.`);
+
+const max = Math.max(10, 20, 30, 40, 50);
+console.log(`The maximum number is ${max}.`);
